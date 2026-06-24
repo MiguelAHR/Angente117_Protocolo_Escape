6f2635a661bc598a4ab0a507b180f344c5696ace
Mahr16 <78241410+MiguelAHR@users.noreply.github.com>
2026-06-24 09:56:06 -0500

    add pasar de nivel and fix levels

---
+++ Assets/Scripts/Menu/PausarJuego.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PausarJuego : MonoBehaviour
+{
+    public GameObject menuPausa;
+    public bool juegoPausado = false;    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+    public void Reanudar()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale = 1;
+        juegoPausado = false;
+    }
+
+    public void Pausar()
+    {
+        menuPausa.SetActive(true);
+        Time.timeScale = 0;
+        juegoPausado = true;
+    }
+
+}
