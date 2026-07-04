07d54798de2ab5335b8edb5eadee3ff671dc548b
Mahr16 <78241410+MiguelAHR@users.noreply.github.com>
2026-07-04 18:22:18 -0500

    update style game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -57,6 +57,7 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         if (gameOverActivo) return;
+
         gameOverActivo = true;
 
         if(gameOverPanel !=null)
